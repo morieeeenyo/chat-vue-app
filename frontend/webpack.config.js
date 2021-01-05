693a025cf5df73c737055b0f3ff9bdca315c2cd7
togo-mori <togo.mori@di-v.co.jp>
2021-01-06 07:49:58 +0900

    test commit

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-    entry: path.resolve(__dirname, "main.js"),
-    output: {
-        path: path.resolve(__dirname, ''),
-        filename: 'bundle.js'
-    }
+  entry: {
+    index: path.join(__dirname, 'src', 'main.js')
+  },
+  output: {
+    path: path.join(__dirname, 'out'),
+    filename: 'index.js'
+  },
+  devtool: 'cheap-module-eval-source-map',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    externalPlugins
+  ]
 }
