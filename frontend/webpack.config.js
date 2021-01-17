3aa89aa4e75227e1d6f7e156672abd1f7f431915
togo-mentor <uint2435@gmail.com>
2021-01-18 08:35:29 +0900

    docs: added comments to webpack config js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
 const { VueLoaderPlugin } = require("vue-loader"); //vue-loaderのインポート
-const path = require('path');
+const path = require('path'); // output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
 module.exports = {
-  mode: "development",
+  mode: "development", // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る。デフォルトではproductionになっている。今回は本番環境で使用しないのでdevelopmentにした
   entry: './src/main.js', //エントリファイル
   output: {
     path: `${__dirname}/../public/javascripts`, //dirnameで現在のパスを絶対パスで指定。ここは絶対パスじゃないとエラーになる
@@ -10,17 +10,18 @@ module.exports = {
   module: {
     rules: [
       { //vueファイルを読み込むための設定
-        test: /\.vue$/,
-        exclude: /node_modules/,
+        test: /\.vue$/, //loaderの処理対象のファイル。vue全て
+        exclude: /node_modules/, //loaderの処理対象から外すファイル
         loader: 'vue-loader'
       },
       { //cssを読み込むための設定
-        test: /\.css$/,
-        exclude: /node_modules/,
+        test: /\.css$/, //loaderの処理対象のファイル。css全て
+        exclude: /node_modules/, //loaderの処理対象から外すファイル
         use: ['style-loader', 'css-loader'],
       },
     ]
   },
+  //webpack-dev-server用設定
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     watchContentBase: true,
