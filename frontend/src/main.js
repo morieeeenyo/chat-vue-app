1ea29eb373f3a097d89a5c03055199fee13c7a2a
togo-mentor <uint2435@gmail.com>
2021-01-20 08:03:01 +0900

    feat: import axios on main.js to use ajax

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
-import Vue from 'vue';
-import App from './App.vue';
+// Vueのインポート
+import Vue from 'vue'
+import App from './App.vue'
+
+// 以下はajaxを行うために必要
+import axios from 'axios' 
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios) 
 
 // App.vueをエントリとしてレンダリング
 new Vue({
