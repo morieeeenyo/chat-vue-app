ff53d67f7769740e2e575f78e1b7e3345f9b718c
togo-mentor <uint2435@gmail.com>
2021-01-20 07:44:23 +0900

    feat: create chat_groups table

--- db/migrate/20210119223953_create_chat_groups.rb
+++ db/migrate/20210119223953_create_chat_groups.rb
@@ -1,7 +1,7 @@
 class CreateChatGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :chat_groups do |t|
-
+      t.string :group_name, null: false, defalut: ""
       t.timestamps
     end
   end
