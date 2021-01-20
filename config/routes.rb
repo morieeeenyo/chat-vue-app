59a769448ad57082c15dd75ebf85213b3925681d
togo-mentor <uint2435@gmail.com>
2021-01-21 06:33:32 +0900

    feat: built api controller for use rails api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  resources :chat_groups, only: :create
+  namespace 'api' do
+    namespace 'v1' do
+      resources :chat_groups, only: :create
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
