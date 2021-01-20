0a72372ba90a55106853d68f4bf72663398a1810
togo-mentor <uint2435@gmail.com>
2021-01-21 06:37:41 +0900

    feat: defined create action to save grourp

--- app/controllers/api/vi/chat_groups_controller.rb
+++ app/controllers/api/vi/chat_groups_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::ChatGroupsController < ApiController
   def create
+    chat_group = ChatGroup.new(group_parans)
+    if chat_group.save 
+      render json: { group: chat_group }, status: :successfully_created 
+    else
+      render json: { errors: chat_group.errors.full_messages }, status: :failed 
+    end
   end
 end
 
