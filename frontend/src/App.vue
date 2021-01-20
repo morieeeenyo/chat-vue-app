b2e5b0721186e8b78ae2068fa99d6b4dce0e1b86
togo-mentor <uint2435@gmail.com>
2021-01-21 07:33:22 +0900

    feat: set vue-route for create action

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,14 +6,40 @@
 </template>
 
 <script>
+// Vueのインポート
+import Vue from 'vue'
+
+// コンポーネントの読み込み
 import Sidebar from './components/Sidebar.vue'
 import ChatContainer from './components/ChatContainer.vue'
+import ModalWindow from './components/side_bar/ModalWindow.vue'
+
+// 以下はajaxを行うために必要
+import axios from 'axios' 
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios) 
+
+// 以下は非同期通信後の遷移先の指定
+import VueRouter from 'vue-router'
+const router = new VueRouter({
+  routes: [
+    { path: '/chat_group/new',  // :id は数値のみに制限する
+    name: 'GroupCreateForm',
+    component: ModalWindow  },
+    { path: '/chat_group/:id(\\d+)',  // :id は数値のみに制限する
+    name: 'ChatView',
+    component: ChatContainer  }
+  ]
+})
+
+Vue.use(VueRouter)
 
   export default {
     components:{
       Sidebar,
-      ChatContainer
-    }
+      ChatContainer,
+    },
+      router
   }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// 以下はajaxを行うために必要
-import axios from 'axios' 
-import VueAxios from 'vue-axios'
-Vue.use(VueAxios, axios) 
-
 // App.vueをエントリとしてレンダリング
 new Vue({
   el: '#app',
