b6dcade82bafff111b45537a8d06aae241487069
togo-mentor <uint2435@gmail.com>
2021-02-12 08:40:01 +0900

    feat: created messages controller and set routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'homes#index'
   namespace 'api' do
     namespace 'v1' do
-      resources :chat_groups, only: [:index, :show, :create, :update, :destroy]
+      resources :chat_groups, only: [:index, :show, :create, :update, :destroy] do
+        resources :messages, only: [:create]
+      end
     end
   end
   mount ActionCable.server => '/cable'
