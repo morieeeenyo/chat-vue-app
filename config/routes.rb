3f9d149ddbb8fcb66bd5ac8749c21cc5f9ec5c92
togo-mentor <uint2435@gmail.com>
2021-02-12 10:01:12 +0900

    fix: deleted messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   root 'homes#index'
   namespace 'api' do
     namespace 'v1' do
-      resources :chat_groups, only: [:index, :show, :create, :update, :destroy] do
-        resources :messages, only: [:create]
-      end
+      resources :chat_groups, only: [:index, :show, :create, :update, :destroy]
     end
   end
   mount ActionCable.server => '/cable'
