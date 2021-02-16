294a6203f62f6b7b7ac75d2f614f2316d366b622
togo-mentor <uint2435@gmail.com>
2021-02-17 07:48:50 +0900

    feat: build factory bot to test post message

--- spec/channels/message_channel_spec.rb
+++ spec/channels/message_channel_spec.rb
@@ -2,13 +2,21 @@ require 'rails_helper'
 
 RSpec.describe MessageChannel, type: :channel do
   before do
-    # `stub_connection`は、渡されたidで
-    # Connectionインスタンスを初期化する 
+    # `stub_connection`は、渡されたidでConnectionインスタンスを初期化する 
     stub_connection 
     @chat_group = create(:chat_group)
   end
 
   describe "subscribe" do
+    context "subscription成功" do
+      it "正しいグループのidがあればsubscriptionに成功する" do
+          subscribe(chat_group_id: @chat_group.id)
+          expect(subscription).to be_confirmed
+          # check particular stream by name
+          expect(subscription).to have_stream_from("message_channel_#{@chat_group.id}")
+      end
+    end
+
     context "subscribe失敗" do
       it "groupのidがないとsubscribeできない" do
         subscribe
@@ -20,18 +28,6 @@ RSpec.describe MessageChannel, type: :channel do
         expect(subscription).to be_rejected
       end
     end
-
-    context "subscription成功" do
-      it "正しいグループのidがあればsubscriptionに成功する" do
-          subscribe(chat_group_id: @chat_group.id)
-          expect(subscription).to be_confirmed
-          # check particular stream by name
-          expect(subscription).to have_stream_from("message_channel_#{@chat_group.id}")
-      end
-      
-    end
-    
     
   end
-  
 end
