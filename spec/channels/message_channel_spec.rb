17b82dc02d3031465d0f6905724d75b29ce9d13b
togo-mentor <uint2435@gmail.com>
2021-02-19 08:46:40 +0900

    test: wrote test case of post method with valid and invalid case

--- spec/channels/message_channel_spec.rb
+++ spec/channels/message_channel_spec.rb
@@ -33,21 +33,37 @@ RSpec.describe MessageChannel, type: :channel do
 
   describe "メッセージの送信" do
     before do
-      @message = build(:message, chat_group_id: @chat_group.id)
+      @message = build(:message, chat_group_id: @chat_group.id) #postメソッド内でメッセージを保存するのでここではbuildするだけ
     end
     
     context "送信成功" do
-      it "グループが選択されていればメッセージが送信できる" do
+      it "グループが選択されている状態でメッセージを送信するとメッセージが一つDBに保存される" do
           subscribe(chat_group_id: @chat_group.id)
           expect(subscription).to be_confirmed
           expect do 
            perform :post, message: @message.text
-          end. to change(Message, :count).by(1)
-          
+          end. to change(Message, :count).by(1) 
       end
-      
+
+      it "グループが選択されている状態でメッセージを送信するとメッセージが接続しているチャネルに配信される" do
+          subscribe(chat_group_id: @chat_group.id)
+          expect(subscription).to be_confirmed
+          expect do 
+           perform :post, message: @message.text
+          end. to have_broadcasted_to("message_channel_#{@chat_group.id}").with{ |data|
+            expect(data['message']['text']).to eq @message.text
+          }
+      end
+    end 
+
+    context "送信失敗" do
+      it "グループが選択されていない状態でメッセージを送信するとメッセージが保存できない" do
+          subscribe(chat_group_id: nil)
+          expect do 
+           perform :post, message: @message.text
+          end. to raise_error RuntimeError #Must be subscribed! というエラーが出ることを検証
+      end
     end
-    
+
   end
-  
 end
