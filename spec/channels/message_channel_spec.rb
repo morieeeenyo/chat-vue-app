04cd9b6dd7d40b46d69f962958400a4afce3c79d
togo-mentor <uint2435@gmail.com>
2021-02-19 08:56:22 +0900

    test: added exopectation with message id because message must be saved

--- spec/channels/message_channel_spec.rb
+++ spec/channels/message_channel_spec.rb
@@ -52,6 +52,7 @@ RSpec.describe MessageChannel, type: :channel do
            perform :post, message: @message.text
           end. to have_broadcasted_to("message_channel_#{@chat_group.id}").with{ |data|
             expect(data['message']['text']).to eq @message.text
+            expect(data['message']['id']).not_to eq nil #idが存在していることを確認
           }
       end
     end 
