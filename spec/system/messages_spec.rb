a1281b5a66055142edee3c89e9a40245c517f490
togo-mentor <uint2435@gmail.com>
2021-02-22 08:26:44 +0900

    test: added valid case of message submit with switching group
    実行環境でよく起きたバグなので検証。

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -7,10 +7,33 @@ RSpec.describe "Messages", type: :system do
 
   describe "メッセージの投稿" do
     context "メッセージの投稿成功" do
-      it "グループが選択されていて、テキストが存在すればメッセージの投稿ができ、即座に選択したグループに反映される" do
+      it "グループが選択されていて、テキストが存在すればメッセージの投稿ができ、即座に選択したグループにのみ反映される" do
         @chat_group.save
         another_group = create(:chat_group, group_name: 'hoge test')
         select_group(@chat_group) # サイドバーからグループを選択し、非同期でグループ情報を取得
+        @message = build(:message)
+        expect {   
+          fill_in "message_input",	with: @message.text
+          sleep 1 
+          click_on '送信'
+          sleep 3
+        }.to change(Message, :count).by(1)
+        expect(page).to have_selector '.message', text: @message.text
+        #グループを移動したときにメッセージが表示されていないことを検証
+        click_link another_group.group_name, href: "#/chat_groups/#{another_group.id}"
+        expect(page).to  have_selector '#group-name', text: another_group.group_name
+        expect(page).not_to have_selector '.message', text: @message.text
+      end
+
+      it "グループを移動してもメッセージの投稿が重複しない" do
+        # 実行環境でよく起きたバグなので検証したい
+        @chat_group.save
+        another_group = create(:chat_group, group_name: 'hoge test')
+        select_group(@chat_group) # サイドバーからグループを選択し、非同期でグループ情報を取得
+        click_link another_group.group_name, href: "#/chat_groups/#{another_group.id}"
+        expect(page).to  have_selector '#group-name', text: another_group.group_name
+        click_link @chat_group.group_name, href: "#/chat_groups/#{@chat_group.id}"
+        expect(page).to  have_selector '#group-name', text: @chat_group.group_name
         @message = build(:message, chat_group_id: @chat_group.id)
         expect {   
           fill_in "message_input",	with: @message.text
